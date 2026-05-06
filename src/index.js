e7098c60680edd0c87a54691366879ef2896f42d
AShakulia <100370039+AShakulia@users.noreply.github.com>
2026-05-06 21:45:22 +0300

    v0.1.2

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import UiKit from './plugins/ui-kit';
-export * from './components';
-export { UiKit };
-export default UiKit;
--- src/plugins/ui-kit.js
+++
@@ -1,9 +0,0 @@
-import '../styles/main.css';
-import UIButton from '@/components/UIButton/UIButton.vue';
-const UiKit = {
-    install(app) {
-        app.component('UIButton', UIButton);
-    }
-};
-export default UiKit;
-export { UIButton };
